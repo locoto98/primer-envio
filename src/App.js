a2e001b4869cde5bae0580218fc2f449cd26fb77
christian torrico avila <christorricoavila@gmail.com>
2021-02-10 07:59:40 -0400

    primero

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+    render() {
+        const { nombre, telefono } = this.props;
+        return (
+            <div>
+            <h1>Hola Christian</h1>
+            <h2>Tu nombre es : {nombre}</h2>
+            <h2>Tu telefono es : {telefono}</h2>
+            <h1>Expresiones {50*50}</h1>
+        </div>
+        )
+    }
 }
-
-export default App;
